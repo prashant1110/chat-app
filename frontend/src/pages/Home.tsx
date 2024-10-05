bbae78ce8510e259b521307de9fc8894a913a056
Prashant Singh <prashant@codetailor.in>
2024-10-05 13:02:57 +0530

    START:establish the frontend

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<div className='flex h-[80vh] w-full md:max-w-screen-md md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<h1>Sidebar</h1>
+			<h1>MessageContainer </h1> 
+		</div>
+	);
+};
+export default Home;
