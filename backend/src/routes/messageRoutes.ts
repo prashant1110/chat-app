fdff99d1ddb353dd4c88fd70356a5b11bd3aa4f3
Prashant Singh <prashant@codetailor.in>
2024-10-05 12:19:31 +0530

    completed backend message route

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import protectedRoute from '../middlewares/protectedRoute.js';
-import { sendMessage } from '../controllers/messageController.js';
+import { getMessage, getUserForSidebar, sendMessage } from '../controllers/messageController.js';
 
 const router=express.Router();
 
+router.get('/conversations',protectedRoute as any,getUserForSidebar)
+router.get('/:id',protectedRoute as any,getMessage as any)
 router.post('/send/:id',protectedRoute as any,sendMessage)
 
 export default router;
